bae1b00d3901f653beac8e6a9a59b09c06fc6d94
ilya.vasiuk <ilya.vasiuk@gmail.com>
2022-10-12 15:28:05 +0200

    gradle

---
+++ solutions/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+  kotlin("jvm").version("1.7.20")
+  application
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+  testImplementation("org.jetbrains.kotlin:kotlin-test")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
