28d1264506d87bd0edc2b8cef8b5500ff2ce788c
ilya.vasiuk <ilya.vasiuk@gmail.com>
2022-10-19 19:50:28 +0200

    1114 Print in Order

---
+++ solutions/src/main/kotlin/by.ivasiuk/leetcode/1114.Print in Order.kt
@@ -0,0 +1,30 @@
+package by.ivasiuk.leetcode
+
+import java.util.concurrent.CountDownLatch
+
+class PrintInOrder {
+  private var firstRan = CountDownLatch(1)
+  private var secondRan = CountDownLatch(1)
+
+  @Throws(InterruptedException::class)
+  fun first(printFirst: Runnable) {
+    // printFirst.run() outputs "first". Do not change or remove this line.
+    printFirst.run()
+    firstRan.countDown()
+  }
+
+  @Throws(InterruptedException::class)
+  fun second(printSecond: Runnable) {
+    firstRan.await()
+    // printSecond.run() outputs "second". Do not change or remove this line.
+    printSecond.run()
+    secondRan.countDown()
+  }
+
+  @Throws(InterruptedException::class)
+  fun third(printThird: Runnable) {
+    secondRan.await()
+    // printThird.run() outputs "third". Do not change or remove this line.
+    printThird.run()
+  }
+}
---
+++ solutions/src/test/kotlin/by/ivasiuk/leetcode/1114.Print in Order.kt
@@ -0,0 +1,19 @@
+package by.ivasiuk.leetcode
+
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+
+class PrintInOrderTest {
+  private val solution = PrintInOrder()
+
+  @Test
+  fun example1() {
+    val sout = mutableSetOf<String>()
+
+    Thread { solution.third { sout.add("third") } }.start()
+    Thread { solution.second { sout.add("second") } }.start()
+    Thread { solution.first { sout.add("first") } }.start()
+
+    assertContentEquals(listOf("first", "second", "third"), sout)
+  }
+}
