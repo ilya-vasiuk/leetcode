8a7065022c9b7ec2c51f5f140e6d05adee1c4f34
ilya.vasiuk <ilya.vasiuk@gmail.com>
2022-10-30 15:50:00 +0100

    1141 User Activity for the Past 30 Days I + 1693 Daily Leads and Partners + 1729 Find Followers Count

---
+++ solutions/src/main/kotlin/by.ivasiuk/leetcode/1141.User Activity for the Past 30 Days I.sql
@@ -0,0 +1,9 @@
+select
+  activity_date as day,
+  count(distinct user_id) as active_users
+from
+  Activity
+where
+  datediff('2019-07-27', activity_date) between 0 and 29
+group by
+  activity_date
