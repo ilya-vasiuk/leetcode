ea243102c884377e23640f176005e2368d88a4c7
ilya.vasiuk <ilya.vasiuk@gmail.com>
2022-11-16 10:59:47 +0100

    976 speed up

--- solutions/src/main/kotlin/by/ivasiuk/leetcode/976.Largest Perimeter Triangle.kt
+++ solutions/src/main/kotlin/by/ivasiuk/leetcode/976.Largest Perimeter Triangle.kt
@@ -8,13 +8,9 @@ class LargestPerimeterTriangle {
   fun largestPerimeter(nums: IntArray): Int {
     val sortedNums = nums.sortedDescending()
 
-    for (a in 0 until sortedNums.size - 2) {
-      for (b in a + 1 until sortedNums.size - 1) {
-        for (c in b + 1 until sortedNums.size) {
-          if (sortedNums[a] < sortedNums[b] + sortedNums[c]) {
-            return sortedNums[a] + sortedNums[b] + sortedNums[c]
-          }
-        }
+    for (i in 0 until sortedNums.size - 2) {
+      if (sortedNums[i] < sortedNums[i + 1] + sortedNums[i + 2]) {
+        return sortedNums[i] + sortedNums[i + 1] + sortedNums[i + 2]
       }
     }
 
