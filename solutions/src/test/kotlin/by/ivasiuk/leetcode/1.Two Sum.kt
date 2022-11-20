e8bb72e08317e0cca9ff8f3fa4464b72da1ecd1c
ilya.vasiuk <ilya.vasiuk@gmail.com>
2022-11-20 14:44:14 +0100

    1 Test refactoring

--- solutions/src/test/kotlin/by/ivasiuk/leetcode/1.Two Sum.kt
+++ solutions/src/test/kotlin/by/ivasiuk/leetcode/1.Two Sum.kt
@@ -8,28 +8,34 @@ class TwoSumTest {
 
   @Test
   fun example1() {
-    val result = solution.twoSum(
-      nums = intArrayOf(2, 7, 11, 15),
-      target = 9,
+    assertContentEquals(
+      expected = intArrayOf(0, 1),
+      actual = solution.twoSum(
+        nums = intArrayOf(2, 7, 11, 15),
+        target = 9,
+      )
     )
-    assertContentEquals(result, intArrayOf(0, 1))
   }
 
   @Test
   fun example2() {
-    val result = solution.twoSum(
-      nums = intArrayOf(3, 2, 4),
-      target = 6,
+    assertContentEquals(
+      expected = intArrayOf(1, 2),
+      actual = solution.twoSum(
+        nums = intArrayOf(3, 2, 4),
+        target = 6,
+      )
     )
-    assertContentEquals(result, intArrayOf(1, 2))
   }
 
   @Test
   fun example3() {
-    val result = solution.twoSum(
-      nums = intArrayOf(3, 3),
-      target = 6,
+    assertContentEquals(
+      expected = intArrayOf(0, 1),
+      actual = solution.twoSum(
+        nums = intArrayOf(3, 3),
+        target = 6,
+      )
     )
-    assertContentEquals(result, intArrayOf(0, 1))
   }
 }
