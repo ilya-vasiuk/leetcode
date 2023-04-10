1980472ac48b417331a873520a00efa10498e012
Ilya Vasiuk <ilya.vasiuk@gmail.com>
2023-04-10 10:35:26 +0200

    20 moved to stack

--- solutions/src/main/kotlin/by/ivasiuk/leetcode/20.Valid Parentheses.kt
+++ solutions/src/main/kotlin/by/ivasiuk/leetcode/20.Valid Parentheses.kt
@@ -1,34 +1,27 @@
 package by.ivasiuk.leetcode
 
+import java.util.Stack
+
 /**
  * @see <a href="https://leetcode.com/problems/valid-parentheses/">
  *   Valid Parentheses</a>
  */
 class ValidParentheses {
   fun isValid(s: String): Boolean {
-    val stack = CharArray(s.length)
-    var tail = -1
+    val stack = Stack<Char>()
+
+    fun isPaired(pair: Char) =
+      stack.isNotEmpty() && stack.pop() == pair
 
     for (c in s) {
-      if (PAIRS.keys.contains(c)) {
-        if (tail == -1 || stack[tail] != PAIRS[c]) {
-          return false
-        } else {
-          tail--
-        }
-      } else {
-        stack[++tail] = c
+      when (c) {
+        '{', '[', '(' -> stack.push(c)
+        '}' -> if (!isPaired('{')) return false
+        ')' -> if (!isPaired('(')) return false
+        ']' -> if (!isPaired('[')) return false
       }
     }
 
-    return tail == -1
-  }
-
-  companion object {
-    private val PAIRS = mapOf(
-      '}' to '{',
-      ']' to '[',
-      ')' to '(',
-    )
+    return stack.isEmpty()
   }
 }
