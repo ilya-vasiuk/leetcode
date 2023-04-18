bbad2c5e9611749c75461036971ff0796f64565b
ilya.vasiuk <ilya.vasiuk@gmail.com>
2023-04-18 11:29:38 +0200

    1768 speed up

--- solutions/src/main/kotlin/by/ivasiuk/leetcode/1768.Merge Strings Alternately.kt
+++ solutions/src/main/kotlin/by/ivasiuk/leetcode/1768.Merge Strings Alternately.kt
@@ -5,20 +5,16 @@ package by.ivasiuk.leetcode
  *   Merge Strings Alternately</a>
  */
 class MergeStringsAlternately {
-  fun mergeAlternately(word1: String, word2: String): String {
-    val minLength = minOf(word1.length, word2.length)
-    val result = CharArray(minLength * 2)
-
-    for (i in 0 until minLength) {
-      result[i * 2] = word1[i]
-      result[i * 2 + 1] = word2[i]
-    }
-
-    return result.joinToString("") +
-        if (word1.length > word2.length) {
-          word1.substring(word2.length until word1.length)
-        } else {
-          word2.substring(word1.length until word2.length)
+  fun mergeAlternately(word1: String, word2: String): String =
+    buildString {
+      for (i in 0 until maxOf(word1.length, word2.length)) {
+        if (i < word1.length) {
+          append(word1[i])
         }
-  }
+
+        if (i < word2.length) {
+          append(word2[i])
+        }
+      }
+    }
 }
