82c912912ef4f1542893c781982d12347701c426
ilya.vasiuk <ilya.vasiuk@gmail.com>
2023-05-26 14:46:40 +0200

    837 New 21 Game

---
+++ solutions/src/main/kotlin/by/ivasiuk/leetcode/837.New 21 Game.kt
@@ -0,0 +1,35 @@
+package by.ivasiuk.leetcode
+
+import kotlin.math.round
+
+/**
+ * @see <a href="https://leetcode.com/problems/new-21-game/">New 21 Game</a>
+ */
+class New21Game {
+  fun new21Game(n: Int, k: Int, maxPts: Int): Double {
+    if (k == 0 || n >= k + maxPts) {
+      return 1.0
+    }
+
+    val probs = DoubleArray(n + 1).also {
+      it[0] = 1.0
+    }
+    var windowSum = 1.0
+    var probability = 0.0
+
+    for (i in 1..n) {
+      probs[i] = windowSum / maxPts
+      if (i < k) {
+        windowSum += probs[i]
+      } else {
+        probability += probs[i]
+      }
+
+      if (i - maxPts >= 0) {
+        windowSum -= probs[i - maxPts]
+      }
+    }
+
+    return round(probability * 100_000) / 100_000
+  }
+}
---
+++ solutions/src/test/kotlin/by/ivasiuk/leetcode/837.New 21 Game.kt
@@ -0,0 +1,45 @@
+package by.ivasiuk.leetcode
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class New21GameTest {
+  private val solution = New21Game()
+
+  @Test
+  fun example1() {
+    assertEquals(
+      expected = 1.0,
+      actual = solution.new21Game(
+        n = 10,
+        k = 1,
+        maxPts = 10,
+      )
+    )
+  }
+
+  @Test
+  fun example2() {
+    assertEquals(
+      expected = 0.6,
+      actual = solution.new21Game(
+        n = 6,
+        k = 1,
+        maxPts = 10,
+      )
+    )
+  }
+
+  @Test
+  fun example3() {
+    assertEquals(
+      expected = 0.73278,
+      actual = solution.new21Game(
+        n = 21,
+        k = 17,
+        maxPts = 10,
+      )
+    )
+
+  }
+}
