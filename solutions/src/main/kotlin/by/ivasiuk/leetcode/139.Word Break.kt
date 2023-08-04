11d7cfef177ec9b8752a7f9735bc54145f405632
ilya.vasiuk <ilya.vasiuk@gmail.com>
2023-08-04 11:00:07 +0200

    139 Word Break

--- solutions/src/main/kotlin/by/ivasiuk/leetcode/139.Word Break.kt
+++ solutions/src/main/kotlin/by/ivasiuk/leetcode/139.Word Break.kt
@@ -3,32 +3,16 @@ package by.ivasiuk.leetcode
 /**
  * @see <a href="https://leetcode.com/problems/word-break/">Word Break</a>
  */
-//TODO: trie optimization
 class WordBreak {
   fun wordBreak(s: String, wordDict: List<String>): Boolean {
-    val n = s.length
-    val visited = BooleanArray(n)
+    val cache = mutableMapOf<Int, Boolean>()
 
-    return check(s, n, 0, wordDict.toSet(), visited)
-  }
-
-  private fun check(s: String, n:Int, i: Int, words: Set<String>, visited: BooleanArray): Boolean {
-    if (i == n) {
-      return true
+    fun dfs(i: Int): Boolean = cache.getOrPut(i) {
+      i >= s.length || wordDict
+        .filter { word -> i + word.lastIndex in s.indices && s.substring(i, i + word.length) == word }
+        .any { word -> dfs(i + word.length) }
     }
 
-    if(visited[i]) {
-      return false
-    }
-
-    visited[i] = true
-
-    for (j in i + 1..n) {
-      if(words.contains(s.substring(i, j)) && check(s, n, j, words, visited)) {
-        return true
-      }
-    }
-
-    return false
+    return dfs(0)
   }
 }
