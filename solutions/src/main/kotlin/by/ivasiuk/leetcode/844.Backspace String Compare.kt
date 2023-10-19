16b871635991a4991e32157a7e67ff70b63efc7e
ilya.vasiuk <ilya.vasiuk@gmail.com>
2023-10-19 11:29:48 +0200

    844 Backspace String Compare

--- solutions/src/main/kotlin/by/ivasiuk/leetcode/844.Backspace String Compare.kt
+++ solutions/src/main/kotlin/by/ivasiuk/leetcode/844.Backspace String Compare.kt
@@ -1,45 +1,28 @@
 package by.ivasiuk.leetcode
 
+import java.util.Stack
+
 /**
  * @see <a href="https://leetcode.com/problems/backspace-string-compare/">
  *   Backspace String Compare</a>
  */
 class BackspaceStringCompare {
-  fun backspaceCompare(s: String, t: String): Boolean {
-    var i = s.length - 1
-    var j = t.length - 1
+  fun backspaceCompare(s: String, t: String): Boolean =
+    backspace(s) == backspace(t)
 
-    while (i >= 0 || j >= 0) {
-      i = backspace(s, i)
-      j = backspace(t, j)
+  private fun backspace(s: String): String {
+    val clean = Stack<Char>()
 
-      if ((i < 0 && j < 0) || i >= 0 && j >= 0 && s[i] == t[j]) {
-        i--
-        j--
+    for (c in s) {
+      if (c == '#') {
+        if (clean.isNotEmpty()) {
+          clean.pop()
+        }
       } else {
-        return false
+        clean.push(c)
       }
     }
 
-    return true
-  }
-
-  private fun backspace(s: String, ind: Int): Int {
-    var i = ind
-    var skip = 0
-
-    while (i >= 0 && s[i] == '#') {
-      while (i >= 0 && s[i] == '#') {
-        skip++
-        i--
-      }
-      while (skip > 0 && i >= 0) {
-        if (s[i] == '#') break
-        skip--
-        i--
-      }
-    }
-
-    return i
+    return clean.joinToString("")
   }
 }
