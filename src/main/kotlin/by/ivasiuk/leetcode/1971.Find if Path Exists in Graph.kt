faca451f8fe7305b977a1671cdd8e22a1ee6da17
ilya.vasiuk <ilya.vasiuk@gmail.com>
2024-04-21 13:33:42 +0200

    1971 Find if Path Exists in Graph

--- src/main/kotlin/by/ivasiuk/leetcode/1971.Find if Path Exists in Graph.kt
+++ src/main/kotlin/by/ivasiuk/leetcode/1971.Find if Path Exists in Graph.kt
@@ -6,34 +6,30 @@ import java.util.LinkedList
  * @see <a href="https://leetcode.com/problems/find-if-path-exists-in-graph/">
  *   Find if Path Exists in Graph</a>
  */
-class FindIfPathExistsInGraph
-{
+class FindIfPathExistsInGraph {
   fun validPath(n: Int, edges: Array<IntArray>, source: Int, destination: Int): Boolean {
-    if (source == destination) {
-      return true
+    val visited = BooleanArray(n) { false }.also {
+      it[source] = true
     }
-
-    val matrix = Array(n) { mutableSetOf<Int>() }
-    edges.forEach {
-      matrix[it[0]].add(it[1])
-      matrix[it[1]].add(it[0])
+    val queue = LinkedList<Int>().apply {
+      offer(source)
     }
-
-    val visited = BooleanArray(n)
-    val queue = LinkedList<Int>()
-    queue.offer(source)
 
     while (queue.isNotEmpty()) {
-      val current = queue.pollFirst()
-      visited[current] = true
+      val current = queue.poll()
 
-      for (i in matrix[current]) {
-        if (!visited[i]) {
-          if (i == destination) {
-            return true
-          }
+      if (current == destination) {
+        return true
+      }
 
-          queue.offer(i)
+      for ((u, v) in edges) {
+        if (u == current && !visited[v]) {
+          queue.offer(v)
+          visited[v] = true
+        }
+        if (v == current && !visited[u]) {
+          queue.offer(u)
+          visited[u] = true
         }
       }
     }
